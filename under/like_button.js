5e400d24c8cdc106c93299367e30518dadb6450f
Aaron Glass <aglass@styleseat.com>
2022-02-10 23:52:43 -0500

    push

--- under/like_button.js
+++ under/like_button.js
@@ -21,5 +21,13 @@ class LikeButton extends React.Component {
   }
 }
 
-const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(LikeButton), domContainer);
+// Find all DOM containers, and render Like buttons into them.
+document.querySelectorAll('.like_button_container')
+    .forEach(domContainer => {
+      // Read the comment ID from a data-* attribute.
+      const testID = parseInt(domContainer.dataset.testID, 10);
+      ReactDOM.render(
+          e(LikeButton, { testID: testID }),
+          domContainer
+      );
+    });
